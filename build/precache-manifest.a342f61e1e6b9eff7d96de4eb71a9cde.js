4b54f6dee00953dec5faa333cc8f3d3a08cce8fa
Ffej Caplan <ffej.caplan@gmail.com>
2020-08-09 18:36:26 -0400

    updated indexhtml file

---
+++ build/precache-manifest.a342f61e1e6b9eff7d96de4eb71a9cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04be39181b9f33229ee68093a8cb0ca6",
+    "url": "/Employee_Directory/index.html"
+  },
+  {
+    "revision": "3ee5dd0ae5909a77a3ce",
+    "url": "/Employee_Directory/static/css/main.2f7734de.chunk.css"
+  },
+  {
+    "revision": "9e0b7393338efb0d5bea",
+    "url": "/Employee_Directory/static/js/2.de1edcb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee_Directory/static/js/2.de1edcb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee5dd0ae5909a77a3ce",
+    "url": "/Employee_Directory/static/js/main.46cbd5f5.chunk.js"
+  },
+  {
+    "revision": "c7a5105fca82924c1143",
+    "url": "/Employee_Directory/static/js/runtime-main.f8d49c23.js"
+  }
+]);
