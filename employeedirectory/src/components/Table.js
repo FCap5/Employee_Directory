6a4a2129e86d141fd496a5e720174006cb8f21dc
Ffej Caplan <ffej.caplan@gmail.com>
2020-08-09 15:48:37 -0400

    finished filter method

--- employeedirectory/src/components/Table.js
+++ employeedirectory/src/components/Table.js
@@ -58,7 +58,26 @@ export default class Table extends Component {
   };
 
   handleClick = (event) => {
-    console.log(event);
+    if (!this.state.check) {
+      let hasDogArray = this.state.employees.filter(
+        (employee) => employee.hasDog === 'Yes'
+      );
+      let hasNoDogArray = this.state.employees.filter(
+        (employee) => employee.hasDog === 'No'
+      );
+      this.setState({
+        employees: hasDogArray,
+        noDog: hasNoDogArray,
+        check: true,
+      });
+    } else {
+      const ogEmployeeList = this.state.employees;
+      this.state.noDog.forEach((dog) => {
+        ogEmployeeList.push(dog);
+      });
+      this.setState({ employees: ogEmployeeList, check: false });
+    }
+    return;
   };
   render() {
     return (
